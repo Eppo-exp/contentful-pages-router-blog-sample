5fb0316234c3797ddbc4abe3ce2df5d41b45bfcc
Greg Huels <greg.huels@geteppo.com>
2024-10-10 06:10:56 -0500

    Apply eppo integration to src/pages/[slug].page.tsx

---
+++ src/pages/utils/eppo-client.ts
@@ -0,0 +1,60 @@
+import type { Attributes } from '@eppo/js-client-sdk-common';
+import { init } from '@eppo/node-server-sdk';
+
+import {
+  EppoExperimentFieldsFragment,
+  PageBlogPostFieldsFragment,
+  RichImageFieldsFragment,
+} from '@src/lib/__generated/sdk';
+
+export type VariationEntry = RichImageFieldsFragment | PageBlogPostFieldsFragment;
+
+const eppoClientPromise = (() => {
+  if (!process.env.EPPO_SDK_KEY) {
+    throw new Error('Missing EPPO_SDK_KEY environment variable');
+  }
+  return init({
+    apiKey: process.env.EPPO_SDK_KEY,
+    assignmentLogger: {
+      logAssignment(assignment) {
+        console.log('EppoClient.assignmentLogger => logAssignment()', assignment);
+      },
+    },
+  });
+})();
+
+async function getEppoClient() {
+  return await eppoClientPromise;
+}
+
+export async function selectExperimentEntry<T extends VariationEntry>(
+  eppoExperiment: EppoExperimentFieldsFragment | null | undefined,
+  subjectKey: string,
+  subjectAttributes: Attributes = {},
+): Promise<T | null> {
+  if (!eppoExperiment) {
+    return null;
+  }
+  const flagKey = eppoExperiment.flagKey;
+  if (!flagKey) {
+    throw new Error(`missing required flagKey for entry ${eppoExperiment.sys.id}`);
+  }
+  const controlVariationEntry = eppoExperiment.controlVariation as T;
+  if (!controlVariationEntry) {
+    throw new Error(`missing required controlVariation for entry ${eppoExperiment.sys.id}`);
+  }
+  const treatmentVariationEntries = (eppoExperiment.treatmentVariationsCollection?.items ??
+    []) as Array<T>;
+  if (!treatmentVariationEntries.length) {
+    throw new Error(
+      `missing required treatmentVariationsCollection for entry ${eppoExperiment.sys.id}`,
+    );
+  }
+  const eppoClient = await getEppoClient();
+  const experiment = {
+    flagKey,
+    controlVariationEntry,
+    treatmentVariationEntries,
+  };
+  return eppoClient.getExperimentContainerEntry(experiment, subjectKey, subjectAttributes);
+}
